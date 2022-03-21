2d06538bc5e0da09567313df75082d4adceaa0c4
Leand3k <theleand3000@gmail.com>
2022-03-21 14:59:09 -0400

    [FIXING] Can't upload yet.

--- models/file.py
+++ models/file.py
@@ -1,24 +1,26 @@
 from sqlalchemy import ForeignKey
 from models.article import Article
 from app import db, ma
+from sqlalchemy_media import Image, ImageAnalyzer, ImageValidator, ImageProcessor
 
 
 class Files(db.Model):
     __tablename__ = 'files'
     idFile = db.Column(db.Integer, primary_key=True)
-    idArticle = db.Column(ForeignKey(Article.idArticle), nullable=False)
+    # idArticle = db.Column(ForeignKey(Article.idArticle), nullable=False)
     filename = db.Column(db.String(50))
     data = db.Column(db.LargeBinary)
+    mimetype = db.Column(db.Text, nullable=False)
 
-    def __init__(self, idArticle, filename, data):
-        self.idArticle = idArticle
+    def __init__(self, filename, data, mimetype):
         self.filename = filename
         self.data = data
+        self.mimetype = mimetype
 
 
 class FilesSchema(ma.Schema):
     class Meta:
-        fields = ('idArticle', 'filename', 'data')
+        fields = ('filename', 'data', 'mimetype')
 
 
 file_schema = FilesSchema()
--- routes/filesRoute.py
+++ routes/filesRoute.py
@@ -1,5 +1,6 @@
-
 from flask import request, jsonify, Blueprint, send_file
+from werkzeug.utils import secure_filename
+
 from models import file
 from app import db
 
@@ -8,12 +9,28 @@ filesRoute = Blueprint('filesRoute', __name__)
 
 @filesRoute.route('/files', methods=['POST'])
 def add_file():
-    file.Files.idArticle = request.form['idArticle']
-    file.Files.filename = request.form['filename']
-    file.Files.data = request.form['file']
+    # file.Files.idArticle = request.form['idArticle']
+    # file.Files.filename = request.form['filename']
+    # file.Files.data = request.files['data']
+    # file.Files.data.save(secure_filename(file.Files.data.filename))
 
-    new_file = file.Files(file.Files.idArticle, file.Files.filename, file.Files.data)
-    db.session.add(new_file)
+    # new_file = file.Files(file.Files.idArticle, file.Files.filename, file.Files.data)
+
+    multimedia = request.files['multimedia']
+    if not multimedia:
+        return 'No picture uploaded', 400
+
+    filename = secure_filename(multimedia.filename)
+    file.Files.filename = filename
+    mimetype = multimedia.mimetype
+    file.Files.mimetype = mimetype
+    file.Files.data = multimedia
+    if not filename:
+        return 'Bad Upload!!', 400
+
+    data = file.Files(file.Files.filename, file.Files.data.read(), file.Files.mimetype)
+
+    db.session.add(data)
     db.session.commit()
 
-    return file.file_schema.jsonify(new_file)
+    return file.file_schema.jsonify(data)
